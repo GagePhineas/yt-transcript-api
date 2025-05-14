285819516a7920ded1b7ab86a8b19e9138da6a95
Shawn <140964181+GagePhineas@users.noreply.github.com>
2025-05-13 23:32:45 -0400

    fix: correct getTranscript import

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const getTranscript = require('youtube-transcript');
+const { getTranscript } = require('youtube-transcript');
+
 
 
 
